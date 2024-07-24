93e4cca13816a952db2f8ba5c09b326fdd6dfb6b
JaineRodrigues <jainer469@gmail.com>
2024-07-23 23:18:24 -0300

    feat: add page with mock

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,11 +2,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Appointments from "./pages/agendamentos";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Agendamentos" element={<Appointments/>} />
     </Routes>
   );
 };

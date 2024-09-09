894ee68b5d7af9cd8935c5fa03dc859859cabef8
jaineRodriguues <jainer469@gmail.com>
2024-09-08 22:41:01 -0300

    feat: adicionando funcionalidade de fechar sidebar

--- src/componentes/SidebarLogged/SidebarLogged.tsx
+++ src/componentes/SidebarLogged/SidebarLogged.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { List, ListItem, Typography } from '@mui/material';
+import React, { useState, useRef, useEffect } from 'react';
+import { List, ListItem, Typography, IconButton } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../layout/AuthContext';
 import {
@@ -9,12 +9,28 @@ import {
   DocumentScanner,
   AttachMoney,
   Settings,
-  AccountCircle 
+  ChevronLeft as ChevronLeftIcon,
+  Menu as MenuIcon,
 } from '@mui/icons-material';
 import './style.css';
 
-const SidebarLogged = () => {
+const SidebarLogged: React.FC = () => {
   const { role, logout } = useAuth();
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const sidebarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   if (!role) {
     return null;
@@ -22,20 +38,33 @@ const SidebarLogged = () => {
 
   const menuItems = getMenuItemsByRole(role);
 
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
-    <div className="sidebar-fixed">
-      <List className="sidebar-list">
-        {menuItems.map((item) => (
-          <ListItem button key={item.text} className="sidebar-item">
-            {React.cloneElement(item.icon, { fontSize: 'small', color: 'black' })}
-            <Typography>
-              <Link to={item.link} className="sidebar-link">
-                {item.text}
-              </Link>
-            </Typography>
-          </ListItem>
-        ))}
-      </List>
+    <div className={`sidebar-fixed ${!isMenuOpen ? 'collapsed' : ''}`} ref={sidebarRef}>
+      <div className="sidebar-header">
+        <IconButton className="menu-button" onClick={toggleMenu}>
+          {isMenuOpen ? <ChevronLeftIcon sx={{ fontSize: 20, color: 'white' }} /> : <MenuIcon sx={{ fontSize: 30, color: 'white' }}/>}
+        </IconButton>
+      </div>
+      <div className="sidebar-content">
+        <List className="sidebar-list">
+          {menuItems.map((item) => (
+            <ListItem button key={item.text} className="sidebar-item">
+              {React.cloneElement(item.icon, { sx: { fontSize: 30, color: 'white' } })}
+              {isMenuOpen && (
+                <Typography>
+                  <Link to={item.link} className="sidebar-link">
+                    {item.text}
+                  </Link>
+                </Typography>
+              )}
+            </ListItem>
+          ))}
+        </List>
+      </div>
       <div className="logout-container">
         <button className="logout-button" onClick={logout}>
           Sair
@@ -45,7 +74,7 @@ const SidebarLogged = () => {
   );
 };
 
-const getMenuItemsByRole = (role) => {
+const getMenuItemsByRole = (role: string) => {
   switch (role) {
     case 'funcionario':
       return [
@@ -57,7 +86,6 @@ const getMenuItemsByRole = (role) => {
         { text: 'Financeiro', link: '/funcionario-dashboard/financeiro', icon: <AttachMoney /> },
         { text: 'Modelos', link: '/funcionario-dashboard/documentos', icon: <DocumentScanner /> },
         { text: 'Documentos', link: '/funcionario-dashboard/documentosbenFunc', icon: <DocumentScanner /> },
-        // { text: 'Chat', link: '/funcionario-dashboard/chat', icon: <DocumentScanner /> }
       ];
     case 'beneficiario':
       return [
@@ -66,8 +94,6 @@ const getMenuItemsByRole = (role) => {
         { text: 'Serviços', link: '/beneficiario-dashboard/servicos', icon: <Settings /> },
         { text: 'Modelos', link: '/beneficiario-dashboard/documentos', icon: <DocumentScanner /> },
         { text: 'Documentos', link: '/beneficiario-dashboard/documentosben', icon: <DocumentScanner /> },
-        // { text: 'Chat', link: '/beneficiario-dashboard/chat', icon: <DocumentScanner /> },
-        //{ text: 'Perfil', link: '/beneficiario-dashboard/profile', icon: <AccountCircle /> } 
       ];
     default:
       return [];
